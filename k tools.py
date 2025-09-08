caf73d427448d1db0d7a7d8c93fc523a06a8fce5
kynoyn <bhavikknown@gmail.com>
2025-09-08 16:22:12 +0530

    Update k tools.py

--- k tools.py
+++ k tools.py
@@ -3,7 +3,7 @@ import requests
 from datetime import datetime, timedelta
 
 # YouTube API Key
-API_KEY = "AIzaSyD9LgOTkc4cVDxNa-NkImuyBfhw2nY-X0w"
+API_KEY = ""
 YOUTUBE_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search"
 YOUTUBE_VIDEO_URL = "https://www.googleapis.com/youtube/v3/videos"
 YOUTUBE_CHANNEL_URL = "https://www.googleapis.com/youtube/v3/channels"
